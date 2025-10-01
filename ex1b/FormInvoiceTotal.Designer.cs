11496291f07483e795b0f1cb0241847dd6fab7c0
Dylan-Lehmann <dylanlehmann18@gmail.com>
2025-10-01 16:28:34 -0500

    Completed Ex 2A

--- ex1b/FormInvoiceTotal.Designer.cs
+++ ex1b/FormInvoiceTotal.Designer.cs
@@ -38,13 +38,22 @@
             label4 = new Label();
             buttonCalculate = new Button();
             buttonExit = new Button();
+            label5 = new Label();
+            textBoxEnterSubtotal = new TextBox();
+            ButtonClear = new Button();
+            textBoxTotalInvoices = new TextBox();
+            label6 = new Label();
+            textBoxAvgInvoices = new TextBox();
+            label7 = new Label();
+            textBoxNmbInvoices = new TextBox();
+            label8 = new Label();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 12F);
-            label1.Location = new Point(111, 36);
+            label1.Location = new Point(12, 68);
             label1.Name = "label1";
             label1.Size = new Size(91, 28);
             label1.TabIndex = 0;
@@ -52,14 +61,16 @@
             // 
             // textBoxSubtotal
             // 
-            textBoxSubtotal.Location = new Point(208, 36);
+            textBoxSubtotal.Location = new Point(192, 72);
             textBoxSubtotal.Name = "textBoxSubtotal";
+            textBoxSubtotal.ReadOnly = true;
             textBoxSubtotal.Size = new Size(136, 27);
             textBoxSubtotal.TabIndex = 1;
+            textBoxSubtotal.TabStop = false;
             // 
             // textBoxTotal
             // 
-            textBoxTotal.Location = new Point(208, 167);
+            textBoxTotal.Location = new Point(192, 200);
             textBoxTotal.Name = "textBoxTotal";
             textBoxTotal.ReadOnly = true;
             textBoxTotal.Size = new Size(136, 27);
@@ -70,7 +81,7 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 12F);
-            label2.Location = new Point(144, 163);
+            label2.Location = new Point(12, 195);
             label2.Name = "label2";
             label2.Size = new Size(58, 28);
             label2.TabIndex = 2;
@@ -78,7 +89,7 @@
             // 
             // textBoxDiscountamount
             // 
-            textBoxDiscountamount.Location = new Point(208, 127);
+            textBoxDiscountamount.Location = new Point(192, 155);
             textBoxDiscountamount.Name = "textBoxDiscountamount";
             textBoxDiscountamount.ReadOnly = true;
             textBoxDiscountamount.Size = new Size(136, 27);
@@ -89,14 +100,15 @@
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI", 12F);
-            label3.Location = new Point(33, 122);
+            label3.Location = new Point(12, 154);
             label3.Name = "label3";
             label3.Size = new Size(169, 28);
             label3.TabIndex = 4;
             label3.Text = "Discount Amount:";
+            // 
             // textBoxDiscountpercent
             // 
-            textBoxDiscountpercent.Location = new Point(208, 80);
+            textBoxDiscountpercent.Location = new Point(192, 113);
             textBoxDiscountpercent.Name = "textBoxDiscountpercent";
             textBoxDiscountpercent.ReadOnly = true;
             textBoxDiscountpercent.Size = new Size(136, 27);
@@ -107,7 +119,7 @@
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI", 12F);
-            label4.Location = new Point(40, 80);
+            label4.Location = new Point(12, 112);
             label4.Name = "label4";
             label4.Size = new Size(162, 28);
             label4.TabIndex = 6;
@@ -125,13 +137,97 @@
             // 
             // buttonExit
             // 
-            buttonExit.Location = new Point(282, 254);
+            buttonExit.Location = new Point(379, 254);
             buttonExit.Name = "buttonExit";
             buttonExit.Size = new Size(91, 32);
-            buttonExit.TabIndex = 3;
+            buttonExit.TabIndex = 4;
             buttonExit.Text = "E&xit";
             buttonExit.UseVisualStyleBackColor = true;
             buttonExit.Click += buttonExit_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F);
+            label5.Location = new Point(12, 30);
+            label5.Name = "label5";
+            label5.Size = new Size(141, 28);
+            label5.TabIndex = 8;
+            label5.Text = "Enter &Subtotal:";
+            // 
+            // textBoxEnterSubtotal
+            // 
+            textBoxEnterSubtotal.Location = new Point(192, 34);
+            textBoxEnterSubtotal.Name = "textBoxEnterSubtotal";
+            textBoxEnterSubtotal.Size = new Size(136, 27);
+            textBoxEnterSubtotal.TabIndex = 1;
+            // 
+            // ButtonClear
+            // 
+            ButtonClear.Location = new Point(279, 254);
+            ButtonClear.Name = "ButtonClear";
+            ButtonClear.Size = new Size(94, 32);
+            ButtonClear.TabIndex = 3;
+            ButtonClear.Text = "Clear";
+            ButtonClear.UseVisualStyleBackColor = true;
+            ButtonClear.Click += ButtonClear_Click;
+            // 
+            // textBoxTotalInvoices
+            // 
+            textBoxTotalInvoices.Location = new Point(514, 91);
+            textBoxTotalInvoices.Name = "textBoxTotalInvoices";
+            textBoxTotalInvoices.ReadOnly = true;
+            textBoxTotalInvoices.Size = new Size(136, 27);
+            textBoxTotalInvoices.TabIndex = 16;
+            textBoxTotalInvoices.TabStop = false;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 12F);
+            label6.Location = new Point(334, 90);
+            label6.Name = "label6";
+            label6.Size = new Size(156, 28);
+            label6.TabIndex = 15;
+            label6.Text = "Total of invoices:";
+            // 
+            // textBoxAvgInvoices
+            // 
+            textBoxAvgInvoices.Location = new Point(514, 133);
+            textBoxAvgInvoices.Name = "textBoxAvgInvoices";
+            textBoxAvgInvoices.ReadOnly = true;
+            textBoxAvgInvoices.Size = new Size(136, 27);
+            textBoxAvgInvoices.TabIndex = 14;
+            textBoxAvgInvoices.TabStop = false;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 12F);
+            label7.Location = new Point(334, 132);
+            label7.Name = "label7";
+            label7.Size = new Size(186, 28);
+            label7.TabIndex = 13;
+            label7.Text = "Average of invoices:";
+            // 
+            // textBoxNmbInvoices
+            // 
+            textBoxNmbInvoices.Location = new Point(514, 50);
+            textBoxNmbInvoices.Name = "textBoxNmbInvoices";
+            textBoxNmbInvoices.ReadOnly = true;
+            textBoxNmbInvoices.Size = new Size(136, 27);
+            textBoxNmbInvoices.TabIndex = 12;
+            textBoxNmbInvoices.TabStop = false;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 12F);
+            label8.Location = new Point(334, 46);
+            label8.Name = "label8";
+            label8.Size = new Size(186, 28);
+            label8.TabIndex = 11;
+            label8.Text = "Number of invoices:";
             // 
             // FormInvoiceTotal
             // 
@@ -139,7 +235,16 @@
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             CancelButton = buttonExit;
-            ClientSize = new Size(429, 348);
+            ClientSize = new Size(664, 308);
+            Controls.Add(textBoxTotalInvoices);
+            Controls.Add(label6);
+            Controls.Add(textBoxAvgInvoices);
+            Controls.Add(label7);
+            Controls.Add(textBoxNmbInvoices);
+            Controls.Add(label8);
+            Controls.Add(ButtonClear);
+            Controls.Add(textBoxEnterSubtotal);
+            Controls.Add(label5);
             Controls.Add(buttonExit);
             Controls.Add(buttonCalculate);
             Controls.Add(textBoxDiscountpercent);
@@ -169,5 +274,14 @@
         private Label label4;
         private Button buttonCalculate;
         private Button buttonExit;
+        private Label label5;
+        private TextBox textBoxEnterSubtotal;
+        private Button ButtonClear;
+        private TextBox textBoxTotalInvoices;
+        private Label label6;
+        private TextBox textBoxAvgInvoices;
+        private Label label7;
+        private TextBox textBoxNmbInvoices;
+        private Label label8;
     }
 }
--- ex1b/FormInvoiceTotal.cs
+++ ex1b/FormInvoiceTotal.cs
@@ -2,9 +2,13 @@ namespace ex1b
 {
     public partial class FormInvoiceTotal : Form
     {
+        int numberOfInvoices;
+        decimal totalOfInvoices;
+        decimal avgOfInvoices;
         public FormInvoiceTotal()
         {
             InitializeComponent();
+           
         }
 
         private void buttonExit_Click(object sender, EventArgs e)
@@ -14,24 +18,43 @@ namespace ex1b
 
         private void buttonCalculate_Click(object sender, EventArgs e)
         {
-            decimal subtotal = Convert.ToDecimal(textBoxSubtotal.Text);
-            decimal discountpercent = 0m;
-            if (subtotal >= 500)
-            {
-                discountpercent = 0.20m;
-            }
-            else if (subtotal >= 250 && subtotal < 500)
-            { discountpercent = 0.15m; }
+            decimal subtotal = Convert.ToDecimal(textBoxEnterSubtotal.Text);
+            decimal discountpercent = 0.25m;
+            decimal discountamount = Math.Round(subtotal * discountpercent, 2);
+            decimal total = Math.Round(subtotal - discountamount, 2);
 
-            else if (subtotal >= 100)
-            {
-                discountpercent = 0.10m;
-            }
-            decimal discountamount = subtotal * discountpercent;
-            decimal total = subtotal - discountamount;
-            textBoxDiscountpercent.Text = discountpercent.ToString();
-            textBoxDiscountamount.Text = discountamount.ToString();
-            textBoxTotal.Text = total.ToString();
+            textBoxSubtotal.Text = subtotal.ToString("c");
+            textBoxDiscountpercent.Text = discountpercent.ToString("p1");
+            textBoxDiscountamount.Text = discountamount.ToString("c");
+            textBoxTotal.Text = total.ToString("c");
+
+            numberOfInvoices += 1; 
+            totalOfInvoices += total;
+            avgOfInvoices = totalOfInvoices / numberOfInvoices;
+
+            textBoxNmbInvoices.Text = numberOfInvoices.ToString();
+            textBoxAvgInvoices.Text = avgOfInvoices.ToString("c");
+            textBoxTotalInvoices.Text = totalOfInvoices.ToString("c");
+
+            textBoxEnterSubtotal.Text = "";
+            textBoxEnterSubtotal.Focus();
+        }
+
+        private void ButtonClear_Click(object sender, EventArgs e)
+        {
+            numberOfInvoices = 0;
+            totalOfInvoices = 0m;
+            avgOfInvoices = 0m;
+
+            textBoxNmbInvoices.Text = "";
+            textBoxAvgInvoices.Text = "";
+            textBoxTotalInvoices.Text = "";
+            textBoxSubtotal.Text = "";
+            textBoxDiscountpercent.Text = "";
+            textBoxDiscountamount.Text = "";
+            textBoxTotal.Text = "";
+
+            textBoxEnterSubtotal.Focus();
         }
     }
 }
